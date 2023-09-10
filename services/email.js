15b48ea5f71761d4d3faeec7c4ea068a4f651967
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-09-10 19:01:52 +0200

    update

---
+++ services/email.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+});
+
+const verificationEmail = async (userEmail, verificationToken) => {
+  const emailOptions = {
+    from: "jan.kowalski@proton.me",
+    to: userEmail,
+    subject: "E-mail verification",
+    html: `Please copy the link to verify your email: http://localhost:3000/api/users/verify/${verificationToken}`,
+  };
+  transport.sendMail(emailOptions).catch((err) => console.log(err));
+};
+module.exports = { verificationEmail };
--- services/email/index.js
+++
@@ -1,9 +0,0 @@
-const sendEmail = require('./sendEmail');
-const verifyEmail = require('./veryfiEmail');
-const resendVerifyEmail = require('./resendVerifyEmail');
-
-module.exports = {
-  sendEmail,
-  verifyEmail,
-  resendVerifyEmail,
-};
