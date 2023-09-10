2b1aaff4f20374a25b5324e1673c32f34a23f940
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-09-10 18:43:21 +0200

    update

---
+++ services/email/index.js
@@ -0,0 +1,9 @@
+const sendEmail = require('./sendEmail');
+const verifyEmail = require('./veryfiEmail');
+const resendVerifyEmail = require('./resendVerifyEmail');
+
+module.exports = {
+  sendEmail,
+  verifyEmail,
+  resendVerifyEmail,
+};
